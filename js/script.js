a4094ea30df09ff62253850defc7e274329e23dd
Youss0203 <youssefhalibi003@gmail.com>
2023-11-14 18:26:06 +0100

    provato a colorare le card

--- js/script.js
+++ js/script.js
@@ -14,13 +14,16 @@ for (let i = 1; i <= 100; i++) {
 
     if (i % 15 == 0) {
         headingCardElement.innerHTML = "FizzBuzz"
+        cardElement.style.color="red"
     }
     else if (i % 3 == 0) {
         headingCardElement.innerHTML = "Fizz"
+        cardElement.style.color="goldenrod"
     }
 
     else if (i % 5 == 0) {
         headingCardElement.innerHTML = "Buzz"
+        cardElement.style.color="green"
     }
 
 }
