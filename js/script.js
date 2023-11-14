410bddbcb40fe9c481466d40abea13d39c3324e0
Youss0203 <youssefhalibi003@gmail.com>
2023-11-14 18:19:19 +0100

    aggiunto bonus

--- js/script.js
+++ js/script.js
@@ -1,14 +1,22 @@
 
+const containerElement = document.querySelector("div.container")
 
+for(let i=1; i <=100; i++){
 
-for(let i=0; i <=100; i++){
+    const cardElement = document.createElement("div")
+    cardElement .className="card"
+
+    const headingCardElement = document.createElement('h2');
+     headingCardElement.innerHTML = i;
+
+     cardElement.appendChild(headingCardElement);
+    containerElement.appendChild(cardElement);
 
     if (i % 15 == 0)
-    console.log("FizzBuzz");
+    headingCardElement.innerHTML= "FizzBuzz";
     else if (i % 3 == 0)
-    console.log("Fizz");
+    headingCardElement.innerHTML= "Fizz";
     else if (i % 5 == 0)
-    console.log("Buzz");
-    else
-    console.log(i);
+    headingCardElement.innerHTML= "Buzz";
+
 }
