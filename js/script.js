3b98e48242bdc34a0697294cac9ad79fb7b302f4
Youss0203 <youssefhalibi003@gmail.com>
2023-11-14 18:23:42 +0100

    sistemato indentazione js

--- js/script.js
+++ js/script.js
@@ -1,22 +1,26 @@
 
 const containerElement = document.querySelector("div.container")
 
-for(let i=1; i <=100; i++){
+for (let i = 1; i <= 100; i++) {
 
     const cardElement = document.createElement("div")
-    cardElement .className="card"
+    cardElement.className = "card"
 
     const headingCardElement = document.createElement('h2');
-     headingCardElement.innerHTML = i;
+    headingCardElement.innerHTML = i;
 
-     cardElement.appendChild(headingCardElement);
+    cardElement.appendChild(headingCardElement);
     containerElement.appendChild(cardElement);
 
-    if (i % 15 == 0)
-    headingCardElement.innerHTML= "FizzBuzz";
-    else if (i % 3 == 0)
-    headingCardElement.innerHTML= "Fizz";
-    else if (i % 5 == 0)
-    headingCardElement.innerHTML= "Buzz";
+    if (i % 15 == 0) {
+        headingCardElement.innerHTML = "FizzBuzz"
+    }
+    else if (i % 3 == 0) {
+        headingCardElement.innerHTML = "Fizz"
+    }
+
+    else if (i % 5 == 0) {
+        headingCardElement.innerHTML = "Buzz"
+    }
 
 }
